c605d883fd94a791577f35cebbbd9f54a0e256dc
Timidan <akinnusotutemitayodaniel@gmail.com>
2026-08-22 10:28:59 +0100

    feat(ui): add squircle surface

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -7,3 +7,5 @@ export { Button } from "./Button.js";
 export type { ButtonProps, ButtonVariant } from "./Button.js";
 export { Skeleton } from "./Skeleton.js";
 export type { SkeletonProps } from "./Skeleton.js";
+export { SquircleCard } from "./SquircleCard.js";
+export type { SquircleCardProps } from "./SquircleCard.js";
